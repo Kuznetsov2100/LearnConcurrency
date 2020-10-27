b8fd4d1350b649ac984e7f7e67392da05ae953fa
handane123 <7983700+handane123@users.noreply.github.com>
2020-10-27 16:23:50 +0800

    add bridge-channel

---
+++ channel-pattern/bridge-channel/bridge-channel.go
@@ -0,0 +1,71 @@
+package main
+
+import "fmt"
+
+func main() {
+	orDone := func(done, c <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				select {
+				case <-done:
+					return
+				case v, ok := <-c:
+					if !ok {
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	bridge := func(done <-chan interface{}, chanStream <-chan <-chan interface{}) <-chan interface{} {
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for {
+				var stream <-chan interface{}
+				select {
+				case <-done:
+					return
+				case maybeStream, ok := <-chanStream:
+					if !ok {
+						return
+					}
+					stream = maybeStream
+				}
+				for val := range orDone(done, stream) {
+					select {
+					case <-done:
+					case valStream <- val:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	genVals := func() <-chan <-chan interface{} {
+		chanStream := make(chan (<-chan interface{}))
+		go func() {
+			defer close(chanStream)
+			for i := 0; i < 10; i++ {
+				stream := make(chan interface{}, 1)
+				stream <- i
+				close(stream)
+				chanStream <- stream
+			}
+		}()
+		return chanStream
+	}
+
+	for val := range bridge(nil, genVals()) {
+		fmt.Println(val)
+	}
+}
